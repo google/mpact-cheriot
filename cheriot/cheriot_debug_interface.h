a1e0d2970d04b2f72fcafe8cfd2b7d768c0f985c
Tor Jeremiassen <torerik@google.com>
2026-04-02 15:16:15 -0500

    Update to newer version of bazel. Uses MODULE.bazel instead of WORKSPACE
    PiperOrigin-RevId: 893663415
    Change-Id: I1ec8a89d2a5e35accc5606fe34393ded65059083

--- cheriot/cheriot_debug_interface.h
+++ cheriot/cheriot_debug_interface.h
@@ -38,14 +38,6 @@ class CheriotDebugInterface : public generic::CoreDebugInterface {
   // length specifies the number of bytes (tags) to read.
   virtual absl::StatusOr<size_t> ReadTagMemory(uint64_t address, void* buf,
                                                size_t length) = 0;
-  // Set a data watchpoint for the given memory range. Any access matching the
-  // given access type (load/store) will halt execution following the completion
-  // of that access.
-  virtual absl::Status SetDataWatchpoint(uint64_t address, size_t length,
-                                         AccessType access_type) = 0;
-  // Clear data watchpoint for the given memory address and access type.
-  virtual absl::Status ClearDataWatchpoint(uint64_t address,
-                                           AccessType access_type) = 0;
   // Set an action point at the given address to execute the specified action.
   virtual absl::StatusOr<int> SetActionPoint(
       uint64_t address, absl::AnyInvocable<void(uint64_t, int)> action) = 0;
